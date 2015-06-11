bb7b821d8b01e33ed1af520c398e16fd81eac7d3
Estefania <estefania.sosa@hotmail.es>
2015-06-11 15:50:07 -0300

    Cosas pendientes de commitear

---
+++ src/PagoElectronico/PagoElectronico/Listados Estadisticos/ListadosEst.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PagoElectronico.Listados_Estadisticos
+{
+    partial class Listados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.cmbListado = new System.Windows.Forms.ComboBox();
+            this.cmbPeriodo = new System.Windows.Forms.ComboBox();
+            this.cmbAño = new System.Windows.Forms.ComboBox();
+            this.lblListados = new System.Windows.Forms.Label();
+            this.lblPeriodo = new System.Windows.Forms.Label();
+            this.lblaño = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(272, 390);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(111, 23);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(13, 184);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(629, 188);
+            this.dgvDatos.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.cmbListado);
+            this.groupBox1.Controls.Add(this.cmbPeriodo);
+            this.groupBox1.Controls.Add(this.cmbAño);
+            this.groupBox1.Controls.Add(this.lblListados);
+            this.groupBox1.Controls.Add(this.lblPeriodo);
+            this.groupBox1.Controls.Add(this.lblaño);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(629, 157);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros de Búsqueda";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(349, 116);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(111, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(168, 116);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(111, 23);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // cmbListado
+            // 
+            this.cmbListado.FormattingEnabled = true;
+            this.cmbListado.Location = new System.Drawing.Point(86, 75);
+            this.cmbListado.Name = "cmbListado";
+            this.cmbListado.Size = new System.Drawing.Size(296, 21);
+            this.cmbListado.TabIndex = 5;
+            // 
+            // cmbPeriodo
+            // 
+            this.cmbPeriodo.FormattingEnabled = true;
+            this.cmbPeriodo.Location = new System.Drawing.Point(379, 31);
+            this.cmbPeriodo.Name = "cmbPeriodo";
+            this.cmbPeriodo.Size = new System.Drawing.Size(215, 21);
+            this.cmbPeriodo.TabIndex = 4;
+            // 
+            // cmbAño
+            // 
+            this.cmbAño.FormattingEnabled = true;
+            this.cmbAño.Location = new System.Drawing.Point(86, 31);
+            this.cmbAño.Name = "cmbAño";
+            this.cmbAño.Size = new System.Drawing.Size(215, 21);
+            this.cmbAño.TabIndex = 3;
+            // 
+            // lblListados
+            // 
+            this.lblListados.AutoSize = true;
+            this.lblListados.Location = new System.Drawing.Point(30, 78);
+            this.lblListados.Name = "lblListados";
+            this.lblListados.Size = new System.Drawing.Size(46, 13);
+            this.lblListados.TabIndex = 2;
+            this.lblListados.Text = "Listados";
+            // 
+            // lblPeriodo
+            // 
+            this.lblPeriodo.AutoSize = true;
+            this.lblPeriodo.Location = new System.Drawing.Point(318, 34);
+            this.lblPeriodo.Name = "lblPeriodo";
+            this.lblPeriodo.Size = new System.Drawing.Size(43, 13);
+            this.lblPeriodo.TabIndex = 1;
+            this.lblPeriodo.Text = "Periodo";
+            // 
+            // lblaño
+            // 
+            this.lblaño.AutoSize = true;
+            this.lblaño.Location = new System.Drawing.Point(30, 34);
+            this.lblaño.Name = "lblaño";
+            this.lblaño.Size = new System.Drawing.Size(26, 13);
+            this.lblaño.TabIndex = 0;
+            this.lblaño.Text = "Año";
+            // 
+            // Listados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 420);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Listados";
+            this.Text = "ListadosEst";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ComboBox cmbListado;
+        private System.Windows.Forms.ComboBox cmbPeriodo;
+        private System.Windows.Forms.ComboBox cmbAño;
+        private System.Windows.Forms.Label lblListados;
+        private System.Windows.Forms.Label lblPeriodo;
+        private System.Windows.Forms.Label lblaño;
+    }
+}
