160c0894f7cb702c756d36671e7a2c0185c507f9
ffusaro <flor_fusaro@hotmail.com>
2015-06-14 20:17:51 -0300

    Additional

--- src/PagoElectronico/PagoElectronico/Listados Estadisticos/ListadosEst.cs
+++ src/PagoElectronico/PagoElectronico/Listados Estadisticos/ListadosEst.cs
@@ -37,218 +37,10 @@ namespace PagoElectronico.Listados_Estadisticos
             cmbPeriodo.Items.Add("Enero-Febrero-Marzo");
             cmbPeriodo.Items.Add("Abril-Mayo-Junio");
             cmbPeriodo.Items.Add("Julio-Agosto-Septiembre");
-            cmbPeriodo.Items.Add("Octubre-Nobiembre-Diciembre");
+            cmbPeriodo.Items.Add("Octubre-Noviembre-Diciembre");
 
             //FIN CARGANDO DATOS INICIALES
 
-            cmbListado.Enabled = false;
-            cmbPeriodo.Enabled = false;
-            btnLimpiar.Enabled = true;
-            btnBuscar.Enabled = false;
-
-        }
-
-        private void btnBuscar_Click(object sender, EventArgs e)
-        {
-
-            //dgvDatos.Rows.Clear();
-
-            if (cmbAño.Text == "")
-            {
-                MessageBox.Show("Elija un año por favor");
-                return;
-            }
-
-            if (cmbPeriodo.Text == "")
-            {
-                MessageBox.Show("Elija un Periodo por favor");
-                return;
-            }
-
-            if (cmbListado.Text == "")
-            {
-                MessageBox.Show("Elija un Listado por favor");
-                return;
-            }
-
-            Conexion con = new Conexion();
-            int inicio;
-            int fin;
-            int anio = Convert.ToInt32(cmbAño.SelectedItem);
-
-            if (cmbPeriodo.Text == "Enero-Febrero-Marzo")
-            {
-                inicio = 1;
-                fin = 3;
-            }
-            else
-            {
-                if (cmbPeriodo.Text == "Abril-Mayo-Junio")
-                {
-                    inicio = 4;
-                    fin = 6;
-                }
-                else
-                {
-                    if (cmbPeriodo.Text == "Julio-Agosto-Septiembre")
-                    {
-                        inicio = 7;
-                        fin = 9;
-                    }
-                    else
-                    {
-                        inicio = 10;
-                        fin = 12;
-                    }
-                }
-            }
-            if (cmbListado.Text == "Clientes con cuentas inhabilitadas por no pagar costos de transaccion")
-            {
-                string query = "PRC_estadistico_cuentas_inhabilitadas";
-
-                con.cnn.Open();
-                SqlCommand command = new SqlCommand(query, con.cnn);
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@desde", inicio);
-                command.Parameters.AddWithValue("@hasta", fin);
-                command.Parameters.AddWithValue("@anio", anio);
-                SqlDataReader lector = command.ExecuteReader();
-
-                if (!lector.Read())
-                {
-                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
-                    con.cnn.Close();
-                    return;
-                }
-                con.cnn.Close();
-                //---------
-                con.cnn.Open();
-                DataTable dtDatos = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(query, con.cnn);
-                da.Fill(dtDatos);
-
-                dgvDatos.DataSource = dtDatos;
-                con.cnn.Close();
-            }
-
-            if (cmbListado.Text == "Cliente con mayor cantidad de comisiones facturadas en sus cuentas")
-            {
-                string query = "PRC_estadistico_comisiones_facturadas";
-
-                con.cnn.Open();
-                SqlCommand command = new SqlCommand(query, con.cnn);
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@desde", inicio);
-                command.Parameters.AddWithValue("@hasta", fin);
-                command.Parameters.AddWithValue("@anio", anio);
-                SqlDataReader lector = command.ExecuteReader();
-
-                if (!lector.Read())
-                {
-                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
-                    con.cnn.Close();
-                    return;
-                }
-                con.cnn.Close();
-
-                con.cnn.Open();
-                DataTable dtDatos = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(query, con.cnn);
-                da.Fill(dtDatos);
-
-                dgvDatos.DataSource = dtDatos;
-                con.cnn.Close();
-
-
-            }
-            if (cmbListado.Text == "Clientes con mayor cantidad de transacciones realizadas entre cuentas propias")
-            {
-                string query = "PRC_estadistico_transacciones_cuentas_propias";
-
-                con.cnn.Open();
-                SqlCommand command = new SqlCommand(query, con.cnn);
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@desde", inicio);
-                command.Parameters.AddWithValue("@hasta", fin);
-                command.Parameters.AddWithValue("@anio", anio);
-                SqlDataReader lector = command.ExecuteReader();
-
-                if (!lector.Read())
-                {
-                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
-                    con.cnn.Close();
-                    return;
-                }
-                con.cnn.Close();
-
-                con.cnn.Open();
-                DataTable dtDatos = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(query, con.cnn);
-                da.Fill(dtDatos);
-
-                dgvDatos.DataSource = dtDatos;
-                con.cnn.Close();
-            }
-
-            if (cmbListado.Text == "Paises con mayor cantidad de movimientos")
-            {
-
-                string query2 = "PRC_estadistico_pais_mas_movimientos";
-
-                con.cnn.Open();
-                SqlCommand command = new SqlCommand(query2, con.cnn);
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@desde", inicio);
-                command.Parameters.AddWithValue("@hasta", fin);
-                command.Parameters.AddWithValue("@anio", anio);
-                SqlDataReader lector2 = command.ExecuteReader();
-
-                if (!lector2.Read())
-                {
-                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
-                    con.cnn.Close();
-                    return;
-                }
-                con.cnn.Close();
-
-                con.cnn.Open();
-                DataTable dtDatos = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(query2, con.cnn);
-                da.Fill(dtDatos);
-
-                dgvDatos.DataSource = dtDatos;
-                con.cnn.Close();
-            }
-
-            if (cmbListado.Text == "Total facturado para los distintos tipos de cuentas")
-            {
-                //int anio = Convert.ToInt32(cmbAño.Text);
-                string query2 = "PRC_estadistico_facturado_tipo_cuentas";
-
-                con.cnn.Open();
-                SqlCommand command = new SqlCommand(query2, con.cnn);
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@desde", inicio);
-                command.Parameters.AddWithValue("@hasta", fin);
-                command.Parameters.AddWithValue("@anio", anio);
-                SqlDataReader lector2 = command.ExecuteReader();
-
-                if (!lector2.Read())
-                {
-                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
-                    con.cnn.Close();
-                    return;
-                }
-                con.cnn.Close();
-
-                con.cnn.Open();
-                DataTable dtDatos = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(query2, con.cnn);
-                da.Fill(dtDatos);
-
-                dgvDatos.DataSource = dtDatos;
-                con.cnn.Close();
-            }
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
@@ -300,6 +92,198 @@ namespace PagoElectronico.Listados_Estadisticos
                 btnBuscar.Enabled = true;
             }
 
+        }
+
+        private void btnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            cmbAño.SelectedItem = null;
+            cmbListado.SelectedItem = null;
+            cmbPeriodo.SelectedItem = null;
+            dgvDatos.DataSource = null;
+        }
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            
+            if (cmbAño.Text == "")
+            {
+                MessageBox.Show("Elija un año por favor");
+                return;
+            }
+
+            if (cmbPeriodo.Text == "")
+            {
+                MessageBox.Show("Elija un Periodo por favor");
+                return;
+            }
+
+            if (cmbListado.Text == "")
+            {
+                MessageBox.Show("Elija un Listado por favor");
+                return;
+            }
+
+            Conexion con = new Conexion();
+            int inicio;
+            int fin;
+            int anio = Convert.ToInt32(cmbAño.SelectedItem);
+
+            if (cmbPeriodo.Text == "Enero-Febrero-Marzo")
+            {
+                inicio = 1;
+                fin = 3;
+            }
+            else
+            {
+                if (cmbPeriodo.Text == "Abril-Mayo-Junio")
+                {
+                    inicio = 4;
+                    fin = 6;
+                }
+                else
+                {
+                    if (cmbPeriodo.Text == "Julio-Agosto-Septiembre")
+                    {
+                        inicio = 7;
+                        fin = 9;
+                    }
+                    else
+                    {
+                        inicio = 10;
+                        fin = 12;
+                    }
+                }
+            }
+            if (cmbListado.Text == "Clientes con cuentas inhabilitadas por no pagar costos de transaccion")
+            {
+                string query = "dbo.PRC_estadistico_cuentas_inhabilitadas";
+
+                con.cnn.Open();
+                SqlCommand command = new SqlCommand(query, con.cnn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@desde", inicio));
+                command.Parameters.Add(new SqlParameter("@hasta", fin));
+                command.Parameters.Add(new SqlParameter("@anio", anio));
+                                
+                SqlDataReader lector = command.ExecuteReader();
+                
+                if (!lector.Read())
+                {
+                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
+                    con.cnn.Close();
+                    return;
+                }
+                DataTable dtDatos = new DataTable();
+                dtDatos.Load(lector);
+                con.cnn.Close();
+                dgvDatos.DataSource = dtDatos;
+                
+            }
+
+            if (cmbListado.Text == "Cliente con mayor cantidad de comisiones facturadas en sus cuentas")
+            {
+                string query = "dbo.PRC_estadistico_comisiones_facturadas";
+
+                con.cnn.Open();
+                SqlCommand command = new SqlCommand(query, con.cnn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@desde", inicio));
+                command.Parameters.Add(new SqlParameter("@hasta", fin));
+                command.Parameters.Add(new SqlParameter("@anio", anio));
+                SqlDataReader lector = command.ExecuteReader();
+
+                if (!lector.Read())
+                {
+                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
+                    con.cnn.Close();
+                    return;
+                }
+                
+                DataTable dtDatos = new DataTable();
+                dtDatos.Load(lector);
+                con.cnn.Close();
+                dgvDatos.DataSource = dtDatos; ;
+
+            }
+            if (cmbListado.Text == "Clientes con mayor cantidad de transacciones realizadas entre cuentas propias")
+            {
+                string query = "dbo.PRC_estadistico_transacciones_cuentas_propias";
+
+                con.cnn.Open();
+                SqlCommand command = new SqlCommand(query, con.cnn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@desde", inicio));
+                command.Parameters.Add(new SqlParameter("@hasta", fin));
+                command.Parameters.Add(new SqlParameter("@anio", anio));
+                SqlDataReader lector = command.ExecuteReader();
+
+                if (!lector.Read())
+                {
+                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
+                    con.cnn.Close();
+                    return;
+                }
+                DataTable dtDatos = new DataTable();
+                dtDatos.Load(lector);
+                con.cnn.Close();
+                dgvDatos.DataSource = dtDatos;
+            }
+
+            if (cmbListado.Text == "Paises con mayor cantidad de movimientos")
+            {
+
+                string query2 = "dbo.PRC_estadistico_pais_mas_movimientos";
+
+                con.cnn.Open();
+                SqlCommand command = new SqlCommand(query2, con.cnn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@desde", inicio));
+                command.Parameters.Add(new SqlParameter("@hasta", fin));
+                command.Parameters.Add(new SqlParameter("@anio", anio));
+                SqlDataReader lector2 = command.ExecuteReader();
+
+                if (!lector2.Read())
+                {
+                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
+                    con.cnn.Close();
+                    return;
+                }
+
+                DataTable dtDatos = new DataTable();
+                dtDatos.Load(lector2);
+                con.cnn.Close();
+                dgvDatos.DataSource = dtDatos;
+            }
+
+            if (cmbListado.Text == "Total facturado para los distintos tipos de cuentas")
+            {
+                //int anio = Convert.ToInt32(cmbAño.Text);
+                string query2 = "dbo.PRC_estadistico_facturado_tipo_cuentas";
+
+                con.cnn.Open();
+                SqlCommand command = new SqlCommand(query2, con.cnn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@desde", inicio));
+                command.Parameters.Add(new SqlParameter("@hasta", fin));
+                command.Parameters.Add(new SqlParameter("@anio", anio));
+                SqlDataReader lector2 = command.ExecuteReader();
+
+                if (!lector2.Read())
+                {
+                    MessageBox.Show("No hay ningun resultado que coincida con las opciones elegidas");
+                    con.cnn.Close();
+                    return;
+                }
+                con.cnn.Close();
+
+                con.cnn.Open();
+                DataTable dtDatos = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(query2, con.cnn);
+                da.Fill(dtDatos);
+
+                dgvDatos.DataSource = dtDatos;
+                con.cnn.Close();
+            }
         }
 
    
