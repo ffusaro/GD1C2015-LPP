2753246609674f4a7e80349ea4e00e5d1130fc81
ffusaro <flor_fusaro@hotmail.com>
2015-06-17 02:06:31 -0300

    Correccion cuentas

--- src/PagoElectronico/PagoElectronico/ABM de Usuario/BuscarUsuario.cs
+++ src/PagoElectronico/PagoElectronico/ABM de Usuario/BuscarUsuario.cs
@@ -16,6 +16,7 @@ namespace PagoElectronico.ABM_de_Usuario
         ABMUsuario FormUsuario;
         ABMCliente FormCliente;
         ABM_Cliente.AsignarUsuario FormAsignar;
+        ABM_Cuenta.AsignarUsuarioCuenta ac;
         public MenuPrincipal mp;
         public DataTable dt;
         public int ev;
@@ -90,9 +91,9 @@ namespace PagoElectronico.ABM_de_Usuario
             }
             else
             {
-                /*ac = new ABM_Cuenta.AsignarUsuario("A", Usuario);
+                ac = new ABM_Cuenta.AsignarUsuarioCuenta("A", Usuario);
                 ac.Show();
-                this.Close();*/
+                this.Close();
             }
 
         }
